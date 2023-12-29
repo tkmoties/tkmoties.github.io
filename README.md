b1ad58a752d93db1a64e89629602103a726a1e5e
nils <nils@nils>
2023-12-29 22:43:52 +0100

    redo data script

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 [tkmoties.github.io](https://tkmoties.github.io)
+
+Funny data:
+- Zaak(f9fe166c-f651-46d9-851e-fdaa2e4a2c30)
+- Zaak(dd16bd1f-31b2-40d0-a2db-73e55c749d9f)
--- motie-cache.js
+++ motie-cache.js
@@ -1,173 +1,127 @@
-console.log("hello world!")
 
 var fs = require('fs');
 
-const o = require('odata').o;
-const odataTk = o('https://gegevensmagazijn.tweedekamer.nl/OData/v4/2.0/')
+const year = process.argv[2];
 
-function testDoc() {
-  odataTk.get('Document(7a9b77f1-d230-4a00-9856-2f0f8e967e62)')
-    .query()
-    .then(function (data) {
-      if (data.Id != '7a9b77f1-d230-4a00-9856-2f0f8e967e62') {
-        console.error(data)
-        throw new Error('Wrong id in test message, expected 7a9b77f1-d230-4a00-9856-2f0f8e967e62, received ' + data.Id);
-      }
-    }).catch(function (data) {
-      console.error(data)
-      throw new Error(`Error fetching test message, status=${data.status}, statusText=${data.statusText}`);
-    });
-}
+const urlApi = 'https://gegevensmagazijn.tweedekamer.nl/OData/v4/2.0';
 
-// testDoc();
+const motieExpand = `
+ZaakActor(
+  $filter=Relatie eq 'Indiener' or Relatie eq 'MedeIndiener'  
+  ;$select=Id,ActorNaam,ActorFractie,GewijzigdOp,ApiGewijzigdOp
+),Besluit(
+  $expand=Stemming(
+    $filter=Verwijderd eq false
+    ;$select=Id,Soort,FractieGrootte,ActorNaam,ActorFractie,GewijzigdOp,ApiGewijzigdOp)
+  ;$filter=Verwijderd eq false and StemmingsSoort ne null
+  ;$select=Id,BesluitSoort,StemmingsSoort,GewijzigdOp,ApiGewijzigdOp
+)`
 
-const year = 2023;
-const filterMoties2023 = `Soort eq 'Motie' and year(GewijzigdOp) eq 2023`;
-var chunkSize = 250;
+// const motieFilter = `
+// Verwijderd eq false and Soort eq 'Motie' and Besluit/any(
+//   a: a/Verwijderd eq false and contains(a/BesluitSoort, 'Stemmen') and not contains(a/BesluitSoort, 'aangehouden')
+// )`
+
+const motieFilter = `
+Verwijderd eq false and Soort eq 'Motie' and Besluit/any(
+  a: a/Verwijderd eq false and a/StemmingsSoort ne null
+)`
+
+
+const motieSelect = `Id,Nummer,Titel,Onderwerp,Kabinetsappreciatie,GestartOp,GewijzigdOp,ApiGewijzigdOp`
+
+const yearFilter = `year(GestartOp) eq ${year}`
+
+const url = `${urlApi}
+/Zaak?
+&$count=true
+&$expand=${motieExpand}
+&$filter=${motieFilter} and ${yearFilter}
+&$select=${motieSelect}`;
+
+// console.log(url);
 
 (async () => {
-  // try {
-    let totalMoties = await odataTk.get(`Zaak/$count`)
-      .query({
-        $filter: filterMoties2023,
+  let nextLink = url;
+  let motieTotal = undefined;
+  let motieCount = 0;
+
+  while (true) {
+    nextLink = await fetch(nextLink)
+      .then(response => {
+        if (response.status != 200) {
+          console.log(response)
+        }
+        return response;
       })
-    totalMoties += totalMoties % chunkSize;
-    console.log(`Totaal aantal moties in ${year}: ${totalMoties}`);
-    
-    // totalMoties = 2;
-    // chunkSize = 2;
-
-    for (var chunkOffset = 0; chunkOffset < totalMoties; chunkOffset += chunkSize) {
-      console.log(chunkOffset);
-      await odataTk.get(`Zaak`)
-        .query({
-          $top: chunkSize,
-          $skip: chunkOffset,
-          $filter: filterMoties2023,
-          $expand: `ZaakActor($filter=Relatie eq 'Indiener' or Relatie eq 'MedeIndiener'),Besluit($expand=Stemming)`
-        })
-        .then(function(moties) {
-          // console.log(moties);
-          for (const motie of moties) {
-            // console.log(motie);
-            var motieToCache = {};
-            motieToCache.Id = motie.Id;
-            motieToCache.Nummer = motie.Nummer;
-            motieToCache.Titel = motie.Titel;
-            motieToCache.Onderwerp = motie.Onderwerp;
-            motieToCache.GewijzigdOp = motie.GewijzigdOp;
-            motieToCache.Kabinetsappreciatie = motie.Kabinetsappreciatie;
-
-            // console.log(`Titel: ${motie.Titel}`)
-            // console.log(`Onderwerp: ${motie.Onderwerp}`)
-            // console.log(`Datum: ${motie.GewijzigdOp}`)
-
-            // console.log('Indiener(s):')
-            // for (const indiener of motie.ZaakActor) {
-            //   console.log(`\t${indiener.ActorNaam} (${indiener.ActorFractie})`)
-            // }
-            motieToCache.Indieners = motie.ZaakActor.map(function(v) { return [v.ActorNaam, v.ActorFractie]; });
-            // const indieners = motie.ZaakActor.reduce(function(a, v) { return a + `${v.ActorNaam} (${v.ActorFractie}); ` }, '');
-            // console.log(`Indieners: ${indieners}`)
-
-            var besluitIdx = 0;
-            motieToCache.Stemmingen = [];
-            for (const besluit of motie.Besluit) {
-              if (besluit.Stemming.length != 0) {
-                var stemmingToCache = {};
-                stemmingToCache.Resultaat = besluit.BesluitTekst;
-                // console.log(`Besluit ${besluitIdx}`)
-                // console.log(`Resultaat: ${besluit.BesluitTekst}`)
-                // console.log(`Datum: ${besluit.GewijzigdOp}`)
-                
-                stemmingToCache.Fracties = {};
-                var totals = {'Voor': 0, 'Tegen': 0, 'Anders': 0};
-                for (const stem of besluit.Stemming) {
-                  stemmingToCache.Fracties[stem.ActorFractie] = stem.Soort;
-
-                  // console.log(`\t\tFractie: ${stem.ActorFractie} (${stem.FractieGrootte})`);
-                  // console.log(`\t\tStem: ${stem.Soort}`);
-                  // console.log(`\t\tDatum: ${stem.GewijzigdOp}`);
-
-                  if (stem.Soort == 'Voor') {
-                    totals['Voor'] += stem.FractieGrootte;
-                  } else if (stem.Soort == 'Tegen') {
-                    totals['Tegen'] += stem.FractieGrootte;
-                  } else {
-                    totals['Anders'] += stem.FractieGrootte;
-                  }
-                }
-                
-                const hoofdelijk = RegExp('\\(Hoofdelijk ([0-9]*)-([0-9]*)\\)', 'gm').exec(besluit.BesluitTekst);
-                if (hoofdelijk != null) {
-                  stemmingToCache.Voor = hoofdelijk[1];
-                  stemmingToCache.Tegen = hoofdelijk[2];
-                  stemmingToCache.Anders = 0;
-                } else {
-                  stemmingToCache.Voor = totals.Voor;
-                  stemmingToCache.Tegen = totals.Tegen;
-                  stemmingToCache.Anders = totals.Anders;
-                }
-                stemmingToCache.PercentVoor = Math.trunc(100 * totals.Voor / (totals.Voor + totals.Tegen));
-                stemmingToCache.PercentTegen = 100 - stemmingToCache.PercentVoor;
-                motieToCache.Stemmingen.push(stemmingToCache);
-              }
-              besluitIdx += 1;
+      .then(response => response.json())
+      .then(odata => {
+        let nextLink = undefined;
+        if (typeof odata['@odata.nextLink'] !== 'undefined') {
+          // If there is more data to retrieve, nextLink is set with the correct $skip value
+          // Remove $count=true from it, only need that once
+          nextLink = odata['@odata.nextLink'].replace('$count=true', '');
+        }
+        if (typeof odata['@odata.count'] !== 'undefined') {
+          motieTotal = odata['@odata.count'];
+        }
+        return [odata.value, nextLink];
+      }).then(args => {
+        let [moties, nextLink] = args;
+        moties.forEach(motie => {
+          motie.Besluit.forEach(besluit => {
+            besluit.Stats = {
+              'Voor': 0,
+              'Tegen': 0,
+              'Anders': 0,
+              'PercentVoor': 0,
+              'PercentTegen': 100,
             }
-            // console.log(motieToCache);
-            // motiesToChache.push(motieToCache);
-            fs.writeFile(`_data/moties/y2023/${motie.GewijzigdOp}-${motie.Id}.json`, JSON.stringify(motieToCache), 'utf8', function(){});
-          }
-          // console.log(moties.length);
-        })
+
+            if (besluit.StemmingsSoort == 'Hoofdelijk') {
+              besluit.Stemming.forEach(stem => {
+                switch (stem.Soort) {
+                  case 'Voor':
+                    besluit.Stats.Voor += 1;
+                    break;
+                  case 'Tegen':
+                    besluit.Stats.Tegen += 1;
+                    break;
+                  default:
+                    besluit.Stats.Anders += 1;
+                    break;
+                }
+              });
+            } else {
+              besluit.Stemming.forEach(stem => {
+                switch (stem.Soort) {
+                  case 'Voor':
+                    besluit.Stats.Voor += stem.FractieGrootte;
+                    break;
+                  case 'Tegen':
+                    besluit.Stats.Tegen += stem.FractieGrootte;
+                    break;
+                  default:
+                    besluit.Stats.Anders += stem.FractieGrootte;
+                    break;
+                }
+              });
+            }
+
+            besluit.Stats.PercentVoor = Math.trunc(100 * besluit.Stats.Voor / (besluit.Stats.Voor + besluit.Stats.Tegen));
+            besluit.Stats.PercentTegen = 100 - besluit.Stats.PercentVoor;
+          })
+          // console.dir(motie, {depth: null});
+          fs.writeFile(
+            `_data/moties/${year}/${motie.GestartOp}-${motie.Id}.json`, 
+            JSON.stringify(motie), 'utf8', (err) => { if (err) throw err });
+        });
+        motieCount += moties.length;
+        console.log(`${motieCount} / ${motieTotal}`)
+        return nextLink;
+      })
+    if (typeof nextLink == 'undefined' || motieCount > motieTotal) {
+      break;
     }
-
-  // } catch (error) {
-  //   console.log(error);
-  //   throw new Error(`Error collecting motie count, status=${error.status}, statusText=${error.statusText}`);
-  // }
-
-  // https://gegevensmagazijn.tweedekamer.nl/OData/v4/2.0/Zaak?$top=1&$filter=Soort eq 'Motie' and year(ApiGewijzigdOp) eq 2023&$orderby=ApiGewijzigdOp desc&ZaakActor($filter=relatie eq 'Indiener' or relatie eq 'MedeIndiener'),Besluit($expand=Stemming)
-  
-    // .then(function(count) {
-    //   console.log(count);
-    // })
+  }
 })();
-
-
-
-
-
-
-// import { o } from 'odata';
-
-// (async () => {
-//   // chaining
-//   const data1 = await o('https://gegevensmagazijn.tweedekamer.nl/OData/v4/2.0')
-//     .get('Document(7a9b77f1-d230-4a00-9856-2f0f8e967e62)')
-//     .query();
-
-//   // handler
-//   const oHandler = o('https://gegevensmagazijn.tweedekamer.nl/OData/v4/2.0');
-//   const data2 = await oHandler
-//     .get('Document(7a9b77f1-d230-4a00-9856-2f0f8e967e62)')
-//     .query();
-//   console.log(data1);
-//   console.log(data2);
-// })();
-
-
-// o().config({
-// 	endpoint: 'https://services.odata.org/V4/(S(ms4wufavzmwsg3fjo3eqdgak))/TripPinServiceRW/'
-// });
-
-// const oHandler = o('https://services.odata.org/V4/(S(ms4wufavzmwsg3fjo3eqdgak))/TripPinServiceRW/', { });
-
-// oHandler.get('People').query().then(function(result) {
-//     console.log(result);
-//     // console.log(result.data);
-// 	// result.data.FirstName = 'New Name';
-// 	// return(result.save());
-// }).then(function(result) {
-//     console.log('First name is now New Name');
-// });
