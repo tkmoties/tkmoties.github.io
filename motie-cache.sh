3b112480d16de9e385e39979069b5000d52b9e0a
nilswiersma <7534520+nilswiersma@users.noreply.github.com>
2025-01-10 17:15:02 +0100

    Update motie-cache.sh

--- motie-cache.sh
+++ motie-cache.sh
@@ -2,7 +2,7 @@ echo "Set up dirs"
 
 mkdir -p _data/moties/
 
-for year in $(seq 2007 2024); do 
+for year in $(seq 2007 2025); do 
   mkdir -p moties/$year/moties;
   cp _templates/year-index.html moties/$year/index.html; 
 done
